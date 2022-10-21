67f6083618cf1f2ca76fbb13b88eadd6adb0d6c9
kylelbs <kyle.lebrissaget@gmail.com>
2022-10-21 23:12:35 +0200

    created a new class to create a 'do'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+class newDo {
+    constructor(task) {
+        this.task = task;
+    }
+};
+
+let do1 = new newDo('lunch');
+let do2 = new newDo('dinner');
+
+console.log(do1.task);
+console.log(do2.task);
